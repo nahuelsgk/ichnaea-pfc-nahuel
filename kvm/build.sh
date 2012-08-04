69dc78a78de9004d75e007142692c5fa59e9d4a0
Miguel Ibero <miguel.ibero.carreras@gmail.com>
2012-08-04 17:13:36 +0200

    updated build script

--- kvm/build.sh
+++ kvm/build.sh
@@ -5,19 +5,24 @@ FILESPATH=files
 while IFS= read -r -u3 -d $'\0' FILE; do
     FILE=`echo $FILE | sed -e "s@$FILESPATH/@@g"`
     FILE=`printf %q "$FILE"`
-    echo "$FILESPATH/$FILE /usr/local/ichnaea/$FILE";
+    echo "$FILESPATH/$FILE /usr/local/ichnaea/$FILE" >> $FILESFILE;
 done 3< <(find $FILESPATH -type f -print0)
 
-#ubuntu-vm-builder kvm precise \
-#                  --domain ichnaea.lsi.upc.edu \
-#                  --arch x86_64 \
-#                  --hostname ichnaea \
-#                  --mem 256 \
-#                  --user ichnaea \
-#                  --pass ichnaea \
-#                  --mirror http://es.archive.ubuntu.com/ubuntu \
-#                  --components main \
-#                  --addpkg 'r-base' \ 
-#                  --addpkg 'r-cran-car' \ 
-#                  --removepkg cron \
-#                  --copy $FILES
+ADD_PACKAGES="r-base r-cran-car"
+REMOVE_PACKAGES="cron"
+
+ubuntu-vm-builder kvm precise \
+                --verbose \
+                --tmp tmp \
+                --flavour virtual \
+                --domain ichnaea.lsi.upc.edu \
+                --arch amd64 \
+                --hostname ichnaea \
+                --mem 256 \
+                --user ichnaea \
+                --pass ichnaea \
+                --mirror http://es.archive.ubuntu.com/ubuntu \
+                --components main,universe \
+                --copy $FILESFILE \
+                --removepkg $REMOVE_PACKAGES \
+                --addpkg $ADD_PACKAGES 
