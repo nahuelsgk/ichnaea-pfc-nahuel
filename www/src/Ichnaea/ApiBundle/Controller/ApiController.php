ed7bf68054d513d7226ff2ace4ff577123e678a3
Nahuel Velazco <nahuel@Rodio@fapac.org>
2013-06-05 17:17:00 +0200

    Adding swiftmailer configuration

--- www/src/Ichnaea/ApiBundle/Controller/ApiController.php
+++ www/src/Ichnaea/ApiBundle/Controller/ApiController.php
@@ -2,31 +2,31 @@
 namespace Ichnaea\ApiBundle\Controller;
 
 use Symfony\Component\BrowserKit\Response;
-
+use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ApiController extends Controller{
+class ApiController extends FosRestController{
     
-    /**
-     * @TODO Documentation and fix controller name
-     */
-    public function getAction($id)
+    
+    public function getSeasonAction($id)
     {   
     	$ichnaeaService = $this->get('ichnaea.service');
-    	return $ichnaeaService->getSeasonById($id);
+    	//$view = $this->view($ichnaeaService->getSeasonById($id), 200);
+    	//return $this->handleView($view);
+    	return $this->view($ichnaeaService->getSeasonById($id), 200);
     }
     
     /**
      * @TODO Documentation and fix controller name
      */
-    public function postAction()
+    public function getSeasonByPatternNameAction()
     {
     	$request = $this->getRequest();
     	$pattern = $request->request->get("pattern");
     	$ichnaeaService = $this->get('ichnaea.service');
-    	return $ichnaeaService->getSeasonByPatterName($pattern);
+    	return $this->view($ichnaeaService->getSeasonByPatterName($pattern), 200);
     }
 
     
