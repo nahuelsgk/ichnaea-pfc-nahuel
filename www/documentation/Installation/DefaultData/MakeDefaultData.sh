05b5eb836f57ed4d9eaaa629aa8d92692180c4d3
Nahuel Velazco <nahuel@Rodio@fapac.org>
2014-05-03 17:49:39 +0200

    Addign jsxls

---
+++ www/documentation/Installation/DefaultData/MakeDefaultData.sh
@@ -0,0 +1,2 @@
+/opt/lampp/bin/mysqldump --add-drop-database -pqwerasdf -u root -pqwerasdf ichnaea variable season season_set season_set_comp matrix variable_matrix_config sample > ./raw_fixtures.sql
+
